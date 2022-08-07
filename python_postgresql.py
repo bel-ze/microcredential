ce0243642feffe54d8394408a79e33c8abde01ef
bel <jinmei21381@gmail.com>
2022-08-06 23:28:54 -0400

    august5th commit

--- python_postgresql.py
+++ python_postgresql.py
@@ -32,7 +32,19 @@ else:
 
 
 def volumeCylinder():
-    print("\n\n------ ACTIVITY 3c: Print between a specific range ------------- ")
-    radius = float(input("Enter a radius: "))
-    height = float(input("Enter the height: "))
+    print("\n\n------ ACTIVITY 3c: Defined and math function: ------------- ")
+    pi = math.pi
+    radius = int(input("Enter a radius: "))
+    height = int(input("Enter the height: "))
+    volume = round(pi * height * radius ** 2,2)
+    print("The volume with radius %s and height %s is %s" %(radius,height,volume))
 
+def rollDice():
+    print("\n\n------ ACTIVITY 3d: Write function that simulate roll the dice ------------- ")
+
+    numRoll = int(input("How many times do you want to roll?: "))
+    for i in range(0,numRoll):
+        numDice = random.randint(1,6)
+        print("Roll %s = %s" %(i+1,numDice))
+    
+    
